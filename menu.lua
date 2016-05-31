2e10046765031146cc3e6e095fdc560a5a05f338
FgpMano <fgpmano@gmail.com>
2016-05-31 18:17:33 -0300

    botoes menu

--- menu.lua
+++ menu.lua
@@ -5,14 +5,16 @@ button = {}
 
 
 function button_spawn(x,y,text,id)
-	table.insert(button,{ x = x, y = y, text = text, id = id})
+	table.insert(button, {x = x,y = y, text = text, id = id})
 end
 
 function button_draw()
 	for i,v in ipairs(button) do
-		love.graphics.draw(menu,0,0,ox,0.75)
-		love.graphics.setColor(255,255,255)
-		love.graphics.print(v.text,v.x,v.y,ox,2)
+
+		love.graphics.setFont(fonte)
+		love.graphics.print(v.text,v.x,v.y)
+
+
 	end
 end
 
@@ -21,6 +23,9 @@ function button_click(x,y)
 		if x > v.x and x < v.x + fonte:getWidth(v.text) and y > v.y and y < v.y + fonte:getHeight() then
 			if v.id == "sair" then
 				love.event.quit()
+			end
+			if v.id == "start" then
+				gamestate = "jogando"
 			end
 		end
 	end
